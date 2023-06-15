b4f20866dd22abed83db10e01ce6e4de4e5d10ea
marlonsalazarg <marlon456_456@hotmail.com>
2023-06-15 18:06:25 -0500

    add: exit message, print score method

--- presenter.rb
+++ presenter.rb
@@ -1,7 +1,18 @@
+require "terminal-table"
 module Presenter
   def print_welcome
     ["###################################",
      "#   Welcome to Clivia Generator   #",
      "###################################"].join("\n")
+  end
+
+  def exit_message
+    puts "Thanks for use Clivia Generator, see you soon!"
+    puts "[Marlon Salazar Goncalves]"
+  end
+
+  def print_score(title, headings, rows)
+    table = Terminal::Table.new(title:, headings:, rows:)
+    puts table
   end
 end
