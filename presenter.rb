468c251966576ed3f82357874e7252f597cfd979
marlonsalazarg <marlon456_456@hotmail.com>
2023-06-15 17:51:50 -0500

    add: module Presenter, method welcome

---
+++ presenter.rb
@@ -0,0 +1,7 @@
+module Presenter
+  def print_welcome
+    ["###################################",
+     "#   Welcome to Clivia Generator   #",
+     "###################################"].join("\n")
+  end
+end
